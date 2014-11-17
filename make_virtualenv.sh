9afe5aa76369ddc3be5e3d3c24771e1478a2929b
Paul Harrison <pfh@logarithmic.net>
2014-11-17 23:06:47 +1100

    changes

---
+++ make_virtualenv.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -u -e
+
+V=$1
+
+virtualenv --clear --system-site-packages $V
+[ -d $V/R ] || mkdir $V/R
+
+echo 'export R_LIBS=$VIRTUAL_ENV/R' >>$V/bin/activate
+
+for LIB in $V/lib/python*
+do
+    echo 'import os,sys; os.environ["R_LIBS"]=sys.prefix+"/R"' >$LIB/sitecustomize.py
+done
+
+echo '. `dirname $0`/activate && '`which R`' $@' >$V/bin/R
+echo '. `dirname $0`/activate && '`which Rscript`' $@' >$V/bin/Rscript
+chmod a+x $V/bin/R $V/bin/Rscript
+
+
+#===================================================================================
+set +u
+. $V/bin/activate
+set -u
+#===================================================================================
+
+
+pip install --upgrade git+git://github.com/Theano/Theano.git
+
+Rscript - <<END
+    install.packages("rPython", repos="http://cran.us.r-project.org")
+    source("http://bioconductor.org/biocLite.R")
+    biocLite("limma")
+END
+
+`dirname $0`/freshen.sh $V
